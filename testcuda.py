be5514ced1e564a64aa61956c971f8bcdca3f574
Mike Vermeer <77960135+MikeCVermeer@users.noreply.github.com>
2023-03-24 17:00:22 +0100

    Delete testcuda.py

--- testcuda.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-# check if tensorflow can see the GPU
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-# check if tensorflow can use the GPU
-tf.debugging.set_log_device_placement(True)
